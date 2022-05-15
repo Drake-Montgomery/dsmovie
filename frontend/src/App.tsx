09f779e32fc2d48b001bfdcb043d7fe8a2d88688
Drake-Montgomery <eder_vip@outlook.com>
2022-05-15 18:26:30 -0300

    useParams

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/Form">
-          <Route path=":ganmeId" element={<Form />} />
+          <Route path=":movieId" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/Pages/Form/index.tsx
+++ frontend/src/Pages/Form/index.tsx
@@ -1,48 +1,15 @@
-// import movieScore from "components/movieScore";
-import { Link } from "react-router-dom";
-import "./style.css";
+import FormCard from "components/FormCard";
+import { useParams  } from "react-router-dom";
 
-function movieCard() {
-  const movie = {
-    id: 1,
-    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "Demon's Souls",
-    count: 2,
-    score: 4.8,
-  };
+function Form() {
 
+  const params = useParams();
 
   return (
     <>
-    <div className="dsmovie-form-container">
-      <img className="dsmovie-movies-card-image" src={movie.image} alt={movie.title} />
-      <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <form className="dsmovie-form">
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="email">Informe seu Email</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </div>
-          <div className="dsmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-          </div>
-        </form>
-          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-        
-      </div>
-    </div>
+      <FormCard movieId={`${params.movieId}`} />
     </>
   );
 }
 
-export default movieCard;
+export default Form;
