23d6fc0578c2241cf3f08670753dd798b09f9035
Drake-Montgomery <eder_vip@outlook.com>
2022-05-15 18:57:32 -0300

    Pagination

--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -29,20 +29,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
-    // const handlePageChange = (newPageNumber: number) => {
-    //     setPageNumber(newPageNumber);
-    // }
-
-    // FORMA ERRADA DE FAZER REQUISIÇÃO
-    // axios.get(`${BASE_URL}/movies?size=12&page=1`)
-    //     .then(response => {
-    //         const data = response.data as MoviePage;
-    //         setPageNumber(data.number);
-    //     });
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}  />
 
             <div className="container">
                 <div className="row">
