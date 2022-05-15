61d05d82ea4d469e50c349ab4421c34d7c5e6a78
Drake-Montgomery <eder_vip@outlook.com>
2022-05-15 17:32:24 -0300

    useState, useEffect

--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -1,19 +1,35 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    // FORMA ERRADA DE FAZER REQUISIÇÃO
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
+    }, []);
+
+    // FORMA ERRADA DE FAZER REQUISIÇÃO
+    // axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    //     .then(response => {
+    //         const data = response.data as MoviePage;
+    //         setPageNumber(data.number);
+    //     });
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
+
 
             <div className="container">
                 <div className="row">
