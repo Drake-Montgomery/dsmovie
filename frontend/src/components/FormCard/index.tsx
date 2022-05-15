46bc205e2076ecd51fba119621acd6a9e5452095
Drake-Montgomery <eder_vip@outlook.com>
2022-05-15 18:45:51 -0300

    Show score

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
